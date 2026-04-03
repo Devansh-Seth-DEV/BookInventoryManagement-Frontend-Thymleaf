932d10c91412ccd4f03eaef129bcbab8bcf6787f
Sahil Madeshiya <168260447+Sa-hil36@users.noreply.github.com>
2026-04-04 00:03:25 +0530

    Add inventory page (#9)
    * add low stock inventory page
    
    * checkout code again
    
    * Check controller
    
    ---------
    
    Co-authored-by: Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
    Co-authored-by: sahil <smadeshiya4@gmail.com>

---
+++ src/main/java/com/bookinventory/ui/thymeleaf/frontend/controller/InventoryController.java
@@ -0,0 +1,42 @@
+package com.bookinventory.ui.thymeleaf.frontend.controller;
+
+import com.bookinventory.ui.thymeleaf.frontend.dto.AvailableInventoryResponseDTO;
+import com.bookinventory.ui.thymeleaf.frontend.dto.InventoryResponseDTO;
+import com.bookinventory.ui.thymeleaf.frontend.dto.LowStockResponseDTO;
+import com.bookinventory.ui.thymeleaf.frontend.dto.CartItemDTO;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Controller
+public class InventoryController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${bims.backend.baseurl}")
+    private String backendBaseUrl;
+
+    @GetMapping("/inventory/low-stock")
+    public String getLowStock(Model model, HttpSession session) {
+
+        LowStockResponseDTO[] lowStock =
+                restTemplate.getForObject(
+                        backendBaseUrl + "/api/inventory/low-stock",
+                        LowStockResponseDTO[].class
+                );
+
+        model.addAttribute("lowStock", lowStock);
+
+        return "low-stock"; // Thymeleaf page
+    }
+}

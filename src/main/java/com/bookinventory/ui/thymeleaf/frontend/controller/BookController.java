9149bfd2bd6b06c21b7e136b4a412abc0615f69b
sourav-pandey-dev <128667235+sourav-pandey-dev@users.noreply.github.com>
2026-04-03 18:52:17 +0530

    Feature/admincontrol (#4)
    * done with login and signup
    
    * adding a new feature called cart
    
    * created the admincontroller
    
    * added AdminBookController
    
    ---------
    
    Co-authored-by: Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>

--- src/main/java/com/bookinventory/ui/thymeleaf/frontend/controller/BookController.java
+++ src/main/java/com/bookinventory/ui/thymeleaf/frontend/controller/BookController.java
@@ -3,16 +3,14 @@ package com.bookinventory.ui.thymeleaf.frontend.controller;
 import com.bookinventory.ui.thymeleaf.frontend.dto.AllBookResponseDTO;
 import com.bookinventory.ui.thymeleaf.frontend.dto.CartItemDTO;
 import jakarta.servlet.http.HttpSession;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
-import com.bookinventory.ui.thymeleaf.frontend.dto.AllBookResponseDTO;
+
+import java.util.List;
 
 @Controller
 public class BookController {
